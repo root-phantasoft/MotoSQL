2a40bbded286777f8d36fd79944647eedf46f422
root-phantasoft <root@phanta-soft.com>
2019-08-06 00:38:48 -0300

    Update RedditConfig.php

--- RedditConfig.php
+++ RedditConfig.php
@@ -5,14 +5,14 @@ class RedditConfig{
     static $ENDPOINT_OAUTH = 'https://oauth.reddit.com';
 
     //access token configuration from https://ssl.reddit.com/prefs/apps
-    static $CLIENT_ID = 'cCA5nq_SWEfu4Q';
-    static $CLIENT_SECRET = 'KRIWyG6cUflM3rOOokYpZ8dDecY';
+    static $CLIENT_ID = '';
+    static $CLIENT_SECRET = '';
 
     // Custom user agent as per API directives
     static $CLIENT_USER_AGENT = 'MotoSQL/0.1 by gnualmafuerte';
 
     // User (bot) credentials
     static $USERNAME = 'motosql';
-    static $PASSWORD = '16atf21mig';
+    static $PASSWORD = '';
 }
 ?>
